ea1fe534fa09d0a685ebe8b670e7875c24531fb4
Gloria4302 <glousaenz@hotmail.com>
2026-05-29 21:55:22 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+
+registerLocaleData(localeEs);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
-registerLocaleData(localeEs);
